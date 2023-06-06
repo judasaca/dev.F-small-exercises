37d13dfcee346daa5eef3371301cb0072aa813ea
judasaca <salamancajd14@gmail.com>
2023-06-05 19:19:44 -0500

    Exercise 4: FOR.

---
+++ javascript-exercises/exercise-4.js
@@ -0,0 +1,131 @@
+//--------------------------------------------
+// Punto 1.
+
+for (let i = 0; i < 51; i++) {
+  console.log(i);
+}
+
+//--------------------------------------------
+// Punto 2.
+
+var pokemons = [
+  "bulbasaur",
+  "ivysaur",
+  "venusaur",
+  "charmander",
+  "charmeleon",
+  "charizard",
+  "squirtle",
+  "wartortle",
+  "blastoise",
+  "caterpie",
+  "metapod",
+  "butterfree",
+  "weedle",
+  "kakuna",
+  "beedrill",
+  "pidgey",
+  "pidgeotto",
+  "pidgeot",
+  "rattata",
+  "raticate",
+  "spearow",
+  "fearow",
+  "ekans",
+  "arbok",
+  "pikachu",
+  "raichu",
+  "sandshrew",
+  "sandslash",
+  "nidoran-f",
+  "nidorina",
+  "nidoqueen",
+  "nidoran-m",
+  "nidorino",
+  "nidoking",
+  "clefairy",
+  "clefable",
+  "vulpix",
+  "ninetales",
+  "jigglypuff",
+  "wigglytuff",
+  "zubat",
+  "golbat",
+  "oddish",
+  "gloom",
+  "vileplume",
+  "paras",
+  "parasect",
+  "venonat",
+  "venomoth",
+  "diglett",
+  "dugtrio",
+  "meowth",
+  "persian",
+  "psyduck",
+  "golduck",
+  "mankey",
+  "primeape",
+  "growlithe",
+  "arcanine",
+  "poliwag",
+  "poliwhirl",
+  "poliwrath",
+  "abra",
+  "kadabra",
+  "alakazam",
+  "machop",
+  "machoke",
+  "machamp",
+  "bellsprout",
+  "weepinbell",
+  "victreebel",
+  "tentacool",
+  "tentacruel",
+  "geodude",
+  "graveler",
+  "golem",
+  "ponyta",
+  "rapidash",
+  "slowpoke",
+  "slowbro",
+  "magnemite",
+  "magneton",
+  "farfetchd",
+  "doduo",
+  "dodrio",
+  "seel",
+  "dewgong",
+  "grimer",
+  "muk",
+  "shellder",
+  "cloyster",
+  "gastly",
+  "haunter",
+  "gengar",
+  "onix",
+  "drowzee",
+  "hypno",
+  "krabby",
+  "kingler",
+  "voltorb",
+];
+let maxNumber = parseInt(
+  prompt("Ingresa un número entre 1 y " + pokemons.length)
+);
+for (let i = 1; i < maxNumber; i++) {
+  if (i % 5 == 0) {
+    console.log(pokemons[i]);
+  }
+}
+
+//----------------------------------------------------
+// Punto 3.
+
+let array = [4, "dos", 8, "tres", 5, 9, 1, "cero"];
+
+for (let i = 0; i < array.length; i++) {
+  if (typeof array[i] === "number") {
+    console.log(array[i]);
+  }
+}
